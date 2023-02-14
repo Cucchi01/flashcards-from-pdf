0b913a3e5eb9f03ebd2bab66a7405dde005900e8
Cucchi01 <cucchi01centr@gmail.com>
2023-02-14 14:48:07 +0100

    Created the skeleton of the application
    Used the QTabWidget to divide the application in the two main
    parts(decks and stats)

---
+++ src/main.py
@@ -0,0 +1,70 @@
+import sys
+from PyQt6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QFormLayout,
+    QGridLayout,
+    QTabWidget,
+    QLineEdit,
+    QDateEdit,
+    QPushButton,
+)
+from PyQt6.QtCore import Qt
+from application_costants import *
+
+
+class MainWindow(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.__set_window_style()
+        self.__set_window_layout()
+        self.show()
+
+    def __set_window_style(self):
+        self.setWindowTitle(APPLICATION_NAME)
+        self.resize(800, 600)
+
+    def __set_window_layout(self):
+        main_layout = QGridLayout(self)
+        self.setLayout(main_layout)
+        tab = self.__get_tab_widget()
+        main_layout.addWidget(tab, 0, 0, 2, 1)
+
+    def __get_tab_widget(self) -> QTabWidget:
+        # create a tab widget
+        tab = QTabWidget(self)
+
+        deck_tab = self.__get_tab_decks()
+        stats_tab = self.__get_tab_stats()
+
+        # add panes to the tab widget
+        tab.addTab(deck_tab, "Decks")
+        tab.addTab(stats_tab, "Stats")
+        return tab
+
+    def __get_tab_decks(self) -> QWidget:
+        # personal page
+        personal_page = QWidget(self)
+        layout = QFormLayout()
+        personal_page.setLayout(layout)
+        layout.addRow("First Name:", QLineEdit(self))
+        layout.addRow("Last Name:", QLineEdit(self))
+        layout.addRow("DOB:", QDateEdit(self))
+        return personal_page
+
+    def __get_tab_stats(self) -> QWidget:
+        # personal page
+        personal_page = QWidget(self)
+        layout = QFormLayout()
+        personal_page.setLayout(layout)
+        layout.addRow("First Name:", QLineEdit(self))
+        layout.addRow("Last Name:", QLineEdit(self))
+        layout.addRow("DOB:", QDateEdit(self))
+        return personal_page
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
