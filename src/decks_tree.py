d26630cfdd8d9a3bb7f86f264f3028d9fa83764e
Cucchi01 <cucchi01centr@gmail.com>
2023-02-26 19:12:14 +0100

    Restructured decks_tree

--- src/decks_tree.py
+++ src/decks_tree.py
@@ -12,21 +12,15 @@ import os
 
 import application_constants
 import pdf_visualization
-from deck_directory import DirectoryEntryFolder, FILE, FOLDER
+from deck_directory import DirectoryEntryFolder, DirectoryEntryFile, FILE, FOLDER
 from update_pdf import update_pdf
 
 
 class DecksStructure(QTreeWidget):
-    def __init__(self, parent, path) -> None:
+    def __init__(self, parent, path: str) -> None:
         super().__init__(parent)
-        self.num_col: int = 4
-        self.col_path: int = self.num_col - 1  # path column is the last
-        self.root_folder: DirectoryEntryFolder = DirectoryEntryFolder(
-            entry_name="root", path=path
-        )
-        self.pdf_window: pdf_visualization.PDFWindowVisualization
-        self.menu_right_click: QMenu
-        self.path_pdf_to_update: str
+
+        self.__set_state_variable(path)
         self.__create_tree()
 
         # app = self.headerItem()
@@ -35,75 +29,50 @@ class DecksStructure(QTreeWidget):
         self.__set_menu_right_click()
         self.itemDoubleClicked.connect(self.__on_emtry_double_clicked)
 
-    def __set_menu_right_click(self) -> None:
-        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
-        self.customContextMenuRequested.connect(self.__open_right_click_menu)
-
-        self.menu_right_click = QMenu("Menu", self)
-
-        update_button = QAction("Update PDF", self.menu_right_click)
-        update_button.setStatusTip(
-            "Update the pdf and change accordingly the question positions"
+    def __set_state_variable(self, path: str) -> None:
+        self.num_col: int = 4
+        self.col_path: int = self.num_col - 1  # path column is the last
+        self.root_folder: DirectoryEntryFolder = DirectoryEntryFolder(
+            entry_name="root", path=path
         )
-        update_button.triggered.connect(self.__update_pdf)
-
-        self.menu_right_click.addAction(update_button)
-
-    def __open_right_click_menu(self, event) -> None:
-        selectedItems = self.selectedItems()
-        if len(selectedItems) > 0:
-            full_path = self.__get_entry_full_path(selectedItems[0])
-            self.path_pdf_to_update = full_path
-            self.menu_right_click.popup(QCursor.pos())
-
-    def __update_pdf(self, event) -> None:
-        update_pdf(self.path_pdf_to_update)
-
-    def __on_emtry_double_clicked(
-        self, entry_pressed: QTreeWidgetItem, col_pressed: int
-    ):
-        full_path: str = self.__get_entry_full_path(entry_pressed)
-        self.pdf_window = pdf_visualization.PDFWindowVisualization(full_path)
-        self.pdf_window.showMaximized()
-
-    def __get_entry_full_path(self, entry: QTreeWidgetItem) -> str:
-        name_col = 0
-        full_name = entry.text(name_col)
-        path_name = entry.text(self.col_path)
-        full_path = os.path.join(path_name, full_name)
-        return full_path
+        self.pdf_window: pdf_visualization.PDFWindowVisualization
+        self.menu_right_click: QMenu
+        self.path_pdf_to_update: str
 
     def __create_tree(self) -> None:
         self.__set_style_tree()
         self.__create_top_level_tree()
 
-    def __set_style_tree(self):
+    def __set_style_tree(self) -> None:
         self.setColumnCount(self.num_col)
         self.setFont(QFont("Calisto MT", 12))
         self.setHeaderLabels(["Name", "Type", "Buttons", "Path"])
         base_width = application_constants.BASE_HOME_WIDTH // (self.num_col)
         self.header().setDefaultSectionSize(base_width)
 
-    def __create_top_level_tree(self):
+    def __create_top_level_tree(self) -> None:
         entries = self.__get_subtree(self.root_folder)
         self.insertTopLevelItems(0, entries)
 
-    def __get_subtree(self, folder: DirectoryEntryFolder):
+    def __get_subtree(self, folder: DirectoryEntryFolder) -> list[QTreeWidgetItem]:
         entries: list[QTreeWidgetItem] = []
 
         folder_path = folder.get_path()
         self.__process_entry(
-            folder_path, folder.get_immediate_decks().values(), entries
+            folder_path, list(folder.get_immediate_decks().values()), entries
         )
         self.__process_entry(
-            folder_path, folder.get_immediate_folders().values(), entries
+            folder_path, list(folder.get_immediate_folders().values()), entries
         )
 
         return entries
 
     def __process_entry(
-        self, folder_path: str, list_entries, entries: list[QTreeWidgetItem]
-    ):
+        self,
+        folder_path: str,
+        list_entries: list[DirectoryEntryFile] | list[DirectoryEntryFolder],
+        entries: list[QTreeWidgetItem],
+    ) -> None:
         for entry in list_entries:
             type = entry.get_type()
             entry_name = entry.get_entry_name()
@@ -120,7 +89,7 @@ class DecksStructure(QTreeWidget):
 
             entries.append(child)
 
-    def __create_entry_file(self, entry_name, path):
+    def __create_entry_file(self, entry_name: str, path: str) -> QTreeWidgetItem:
         ext = entry_name.split(".")[-1].upper()
         child = QTreeWidgetItem([entry_name, ext, "", path])
 
@@ -129,3 +98,45 @@ class DecksStructure(QTreeWidget):
         child.setIcon(2, icon)
 
         return child
+
+    def __set_menu_right_click(self) -> None:
+        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.__open_right_click_menu)
+
+        self.menu_right_click = QMenu("Menu", self)
+
+        update_button = QAction("Update PDF", self.menu_right_click)
+        update_button.setStatusTip(
+            "Update the pdf and change accordingly the question positions"
+        )
+        update_button.triggered.connect(self.__update_pdf)
+
+        self.menu_right_click.addAction(update_button)
+
+    def __open_right_click_menu(self, event) -> None:
+        selected_items = self.selectedItems()
+
+        if self.__is_valid_click(selected_items):
+            full_path = self.__get_entry_full_path(selected_items[0])
+            self.path_pdf_to_update = full_path
+            self.menu_right_click.popup(QCursor.pos())
+
+    def __is_valid_click(self, selected_items: list[QTreeWidgetItem]) -> bool:
+        return len(selected_items) > 0
+
+    def __update_pdf(self, event) -> None:
+        update_pdf(self.path_pdf_to_update)
+
+    def __on_emtry_double_clicked(
+        self, entry_pressed: QTreeWidgetItem, col_pressed: int
+    ) -> None:
+        full_path: str = self.__get_entry_full_path(entry_pressed)
+        self.pdf_window = pdf_visualization.PDFWindowVisualization(full_path)
+        self.pdf_window.showMaximized()
+
+    def __get_entry_full_path(self, entry: QTreeWidgetItem) -> str:
+        name_col = 0
+        full_name = entry.text(name_col)
+        path_name = entry.text(self.col_path)
+        full_path = os.path.join(path_name, full_name)
+        return full_path
