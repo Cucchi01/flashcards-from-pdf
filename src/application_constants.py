318d3b0f6db5b0b7ef13aaa4adda2a019b546264
Cucchi01 <cucchi01centr@gmail.com>
2023-09-13 11:13:57 +0200

    Fix: change path for packaging
    Before it was not compatible with the file structure after the packaging

--- src/application_constants.py
+++ src/application_constants.py
@@ -9,6 +9,11 @@ PATH_TO_DECKS_FROM_SRC = "../data/"
 PATH_TO_DECKS_ABS = os.path.abspath(
     os.path.join(os.path.dirname(__file__), PATH_TO_DECKS_FROM_SRC)
 )
+if os.path.exists(PATH_TO_DECKS_ABS) == False:
+    PATH_TO_DECKS_FROM_SRC = "./data/"
+    PATH_TO_DECKS_ABS = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), PATH_TO_DECKS_FROM_SRC)
+    )
 BASE_HOME_WIDTH = 800
 BASE_HOME_HEIGHT = 600
 # TODO: make it choose the right language for the pdf
