54f30624bf11245242359e0940e1185479fd1ba9
Cucchi01 <cucchi01centr@gmail.com>
2023-09-14 08:59:46 +0200

    Feat: change Anki media folder initialization
    Now, it is possible to simply change a txt file in "data\" to change the media folder position. In this way, it can be changed after the packaging

--- src/application_constants.py
+++ src/application_constants.py
@@ -1,12 +1,11 @@
 from PyQt6.QtGui import QFont
 
 import os
+from io import TextIOWrapper
 
-from private_application_constants import PRIVATE_ANKI_CONTENT_DIRECTORY
-
-APPLICATION_NAME = "Flashcards from PDF"
-PATH_TO_DECKS_FROM_SRC = "../data/"
-PATH_TO_DECKS_ABS = os.path.abspath(
+APPLICATION_NAME: str = "Flashcards from PDF"
+PATH_TO_DECKS_FROM_SRC: str = "../data/"
+PATH_TO_DECKS_ABS: str = os.path.abspath(
     os.path.join(os.path.dirname(__file__), PATH_TO_DECKS_FROM_SRC)
 )
 if os.path.exists(PATH_TO_DECKS_ABS) == False:
@@ -14,19 +13,23 @@ if os.path.exists(PATH_TO_DECKS_ABS) == False:
     PATH_TO_DECKS_ABS = os.path.abspath(
         os.path.join(os.path.dirname(__file__), PATH_TO_DECKS_FROM_SRC)
     )
-BASE_HOME_WIDTH = 800
-BASE_HOME_HEIGHT = 600
+BASE_HOME_WIDTH: int = 800
+BASE_HOME_HEIGHT: int = 600
 # TODO: make it choose the right language for the pdf
-LANGUAGE = "italian"
-APPLICATION_STYLE = 'QWidget {font: "Calisto MT", size: 14}'
-FONT = QFont("Calisto MT", 14)
+LANGUAGE: str = "italian"
+APPLICATION_STYLE: str = 'QWidget {font: "Calisto MT", size: 14}'
+FONT: QFont = QFont("Calisto MT", 14)
 
 # file
-TYPE_QUEST_GENERAL_CONSTANT = "g"
-TYPE_QUEST_PAGE_SPECIFIC_CONSTANT = "p"
-ONGOING_TEST_FLAG_YES = "1"
-ONGOING_TEST_FLAG_NO = "0"
-NO_ANSWER_FLAG = "!-!"
-FILE_FLASHCARDS_SEPARATOR = " ?^? "
-ANKI_FLASHCARDS_SEPARATOR = "\t"
-ANKI_CONTENT_DIRECTORY = PRIVATE_ANKI_CONTENT_DIRECTORY
+TYPE_QUEST_GENERAL_CONSTANT: str = "g"
+TYPE_QUEST_PAGE_SPECIFIC_CONSTANT: str = "p"
+ONGOING_TEST_FLAG_YES: str = "1"
+ONGOING_TEST_FLAG_NO: str = "0"
+NO_ANSWER_FLAG: str = "!-!"
+FILE_FLASHCARDS_SEPARATOR: str = " ?^? "
+ANKI_FLASHCARDS_SEPARATOR: str = "\t"
+
+file: TextIOWrapper
+ANKI_CONTENT_DIRECTORY: str = ""
+with open(PATH_TO_DECKS_ABS + "\\private_folder_anki.txt") as file:
+    ANKI_CONTENT_DIRECTORY = file.readline().strip(" \n")
