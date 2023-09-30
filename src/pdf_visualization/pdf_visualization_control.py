58d06613231e9d8c24372beb3f7cb0c7cf7b19cf
Cucchi01 <cucchi01centr@gmail.com>
2023-09-30 12:51:04 +0200

    Chore: copyright notice in each file

--- src/pdf_visualization/pdf_visualization_control.py
+++ src/pdf_visualization/pdf_visualization_control.py
@@ -1,3 +1,11 @@
+# Copyright 2022, 2023 Andrea Cucchietti
+# This file is part of flashcards-from-pdf.
+
+# flashcards-from-pdf is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+# flashcards-from-pdf is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with flashcards-from-pdf. If not, see <https://www.gnu.org/licenses/>.
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QShortcut, QKeySequence
